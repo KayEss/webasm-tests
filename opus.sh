d06ed46a3936149aec9e486bc8b4e36ad2f82661
Kirit Sælensminde <kirit@kirit.com>
2021-11-18 18:51:42 +0700

    Compile and link decoder initialisation

--- opus.sh
+++ opus.sh
@@ -1,24 +1,135 @@
 #!/usr/bin/env bash
+set -ex
+
+./stdlib/stdlib.sh
 
 clang \
     --target=wasm32 \
     -nostdlib -ffreestanding -fno-exceptions \
-    -D_LIBCPP_HAS_NO_THREADS \
+    -emit-llvm -c \
     -DOPUS_BUILD=1 \
     -DUSE_ALLOCA=1 \
     -O3 \
     -I/usr/lib/llvm-13/include/c++/v1/ \
     -I/usr/include \
     -I/usr/include/x86_64-linux-gnu/ \
-    -Wl,--no-entry \
-    -Wl,--export=opus_decoder_create \
+    -Ilibs/opus/celt \
+    -Ilibs/opus/include \
+    -Ilibs/opus/silk \
+    libs/opus/celt/celt.c \
+    -o celt.bc
+
+clang \
+    --target=wasm32 \
+    -nostdlib -ffreestanding -fno-exceptions \
+    -emit-llvm -c \
+    -DOPUS_BUILD=1 \
+    -DUSE_ALLOCA=1 \
+    -O3 \
+    -I/usr/lib/llvm-13/include/c++/v1/ \
+    -I/usr/include \
+    -I/usr/include/x86_64-linux-gnu/ \
+    -Ilibs/opus/celt \
+    -Ilibs/opus/include \
+    -Ilibs/opus/silk \
+    libs/opus/silk/CNG.c \
+    -o CNG.bc
+
+clang \
+    --target=wasm32 \
+    -nostdlib -ffreestanding -fno-exceptions \
+    -emit-llvm -c \
+    -DOPUS_BUILD=1 \
+    -DUSE_ALLOCA=1 \
+    -O3 \
+    -I/usr/lib/llvm-13/include/c++/v1/ \
+    -I/usr/include \
+    -I/usr/include/x86_64-linux-gnu/ \
+    -Ilibs/opus/celt \
+    -Ilibs/opus/include \
+    -Ilibs/opus/silk \
+    libs/opus/celt/celt_decoder.c \
+    -o celt_decoder.bc
+
+clang \
+    --target=wasm32 \
+    -nostdlib -ffreestanding -fno-exceptions \
+    -emit-llvm -c \
+    -DOPUS_BUILD=1 \
+    -DUSE_ALLOCA=1 \
+    -O3 \
+    -I/usr/lib/llvm-13/include/c++/v1/ \
+    -I/usr/include \
+    -I/usr/include/x86_64-linux-gnu/ \
+    -Ilibs/opus/celt \
+    -Ilibs/opus/include \
+    -Ilibs/opus/silk \
+    libs/opus/silk/dec_API.c \
+    -o dec_API.bc
+
+clang \
+    --target=wasm32 \
+    -nostdlib -ffreestanding -fno-exceptions \
+    -emit-llvm -c \
+    -DOPUS_BUILD=1 \
+    -DUSE_ALLOCA=1 \
+    -O3 \
+    -I/usr/lib/llvm-13/include/c++/v1/ \
+    -I/usr/include \
+    -I/usr/include/x86_64-linux-gnu/ \
+    -Ilibs/opus/celt \
+    -Ilibs/opus/include \
+    -Ilibs/opus/silk \
+    libs/opus/silk/init_decoder.c \
+    -o init_decoder.bc
+
+clang \
+    --target=wasm32 \
+    -nostdlib -ffreestanding -fno-exceptions \
+    -emit-llvm -c \
+    -DOPUS_BUILD=1 \
+    -DUSE_ALLOCA=1 \
+    -O3 \
+    -I/usr/lib/llvm-13/include/c++/v1/ \
+    -I/usr/include \
+    -I/usr/include/x86_64-linux-gnu/ \
+    -Ilibs/opus/celt \
+    -Ilibs/opus/include \
+    -Ilibs/opus/silk \
+    libs/opus/celt/modes.c \
+    -o modes.bc
+
+clang \
+    --target=wasm32 \
+    -nostdlib -ffreestanding -fno-exceptions \
+    -emit-llvm -c \
+    -DOPUS_BUILD=1 \
+    -DUSE_ALLOCA=1 \
+    -O3 \
+    -I/usr/lib/llvm-13/include/c++/v1/ \
+    -I/usr/include \
+    -I/usr/include/x86_64-linux-gnu/ \
     -Ilibs/opus/celt \
     -Ilibs/opus/include \
     -Ilibs/opus/silk \
     libs/opus/src/opus_decoder.c \
-    memset.cpp \
-    -o opus.wasm
+    -o opus_decoder.bc
 
+clang \
+    --target=wasm32 \
+    -nostdlib -ffreestanding -fno-exceptions \
+    -emit-llvm -c \
+    -DOPUS_BUILD=1 \
+    -DUSE_ALLOCA=1 \
+    -O3 \
+    -I/usr/lib/llvm-13/include/c++/v1/ \
+    -I/usr/include \
+    -I/usr/include/x86_64-linux-gnu/ \
+    -Ilibs/opus/celt \
+    -Ilibs/opus/include \
+    -Ilibs/opus/silk \
+    libs/opus/silk/PLC.c \
+    -o PLC.bc
 
 clang++ \
     --target=wasm32 \
@@ -37,6 +148,7 @@ clang++ \
     -Wl,--export=free_decoder \
     -Wl,--lto-O3 \
     -Wl,-z,stack-size=$[8 * 1024 * 1024] \
-    atexit.cpp new.cpp \
-    opus.cpp opus.wasm \
+    stdlib/atexit.bc stdlib/malloc.bc stdlib/memset.bc stdlib/new.bc \
+    celt.bc celt_decoder.bc CNG.bc dec_API.bc init_decoder.bc opus_decoder.bc modes.bc PLC.bc \
+    ./opus.cpp \
     -o opus.wasm
