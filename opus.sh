0281b64adf75e39edce91c43d7590ae1d26f03e2
Kirit Sælensminde <kirit@kirit.com>
2021-11-18 20:42:29 +0700

    This looks like the complete set of files needed for OPUS decoding.

--- opus.sh
+++ opus.sh
@@ -3,133 +3,89 @@ set -ex
 
 ./stdlib/stdlib.sh
 
-clang \
-    --target=wasm32 \
-    -nostdlib -ffreestanding -fno-exceptions \
-    -emit-llvm -c \
-    -DOPUS_BUILD=1 \
-    -DUSE_ALLOCA=1 \
-    -O3 \
-    -I/usr/lib/llvm-13/include/c++/v1/ \
-    -I/usr/include \
-    -I/usr/include/x86_64-linux-gnu/ \
-    -Ilibs/opus/celt \
-    -Ilibs/opus/include \
-    -Ilibs/opus/silk \
-    libs/opus/celt/celt.c \
-    -o celt.bc
+mkdir -p celt silk src
 
-clang \
-    --target=wasm32 \
-    -nostdlib -ffreestanding -fno-exceptions \
-    -emit-llvm -c \
-    -DOPUS_BUILD=1 \
-    -DUSE_ALLOCA=1 \
-    -O3 \
-    -I/usr/lib/llvm-13/include/c++/v1/ \
-    -I/usr/include \
-    -I/usr/include/x86_64-linux-gnu/ \
-    -Ilibs/opus/celt \
-    -Ilibs/opus/include \
-    -Ilibs/opus/silk \
-    libs/opus/silk/CNG.c \
-    -o CNG.bc
-
-clang \
-    --target=wasm32 \
-    -nostdlib -ffreestanding -fno-exceptions \
-    -emit-llvm -c \
-    -DOPUS_BUILD=1 \
-    -DUSE_ALLOCA=1 \
-    -O3 \
-    -I/usr/lib/llvm-13/include/c++/v1/ \
-    -I/usr/include \
-    -I/usr/include/x86_64-linux-gnu/ \
-    -Ilibs/opus/celt \
-    -Ilibs/opus/include \
-    -Ilibs/opus/silk \
-    libs/opus/celt/celt_decoder.c \
-    -o celt_decoder.bc
-
-clang \
-    --target=wasm32 \
-    -nostdlib -ffreestanding -fno-exceptions \
-    -emit-llvm -c \
-    -DOPUS_BUILD=1 \
-    -DUSE_ALLOCA=1 \
-    -O3 \
-    -I/usr/lib/llvm-13/include/c++/v1/ \
-    -I/usr/include \
-    -I/usr/include/x86_64-linux-gnu/ \
-    -Ilibs/opus/celt \
-    -Ilibs/opus/include \
-    -Ilibs/opus/silk \
-    libs/opus/silk/dec_API.c \
-    -o dec_API.bc
-
-clang \
-    --target=wasm32 \
-    -nostdlib -ffreestanding -fno-exceptions \
-    -emit-llvm -c \
-    -DOPUS_BUILD=1 \
-    -DUSE_ALLOCA=1 \
-    -O3 \
-    -I/usr/lib/llvm-13/include/c++/v1/ \
-    -I/usr/include \
-    -I/usr/include/x86_64-linux-gnu/ \
-    -Ilibs/opus/celt \
-    -Ilibs/opus/include \
-    -Ilibs/opus/silk \
-    libs/opus/silk/init_decoder.c \
-    -o init_decoder.bc
-
-clang \
-    --target=wasm32 \
-    -nostdlib -ffreestanding -fno-exceptions \
-    -emit-llvm -c \
-    -DOPUS_BUILD=1 \
-    -DUSE_ALLOCA=1 \
-    -O3 \
-    -I/usr/lib/llvm-13/include/c++/v1/ \
-    -I/usr/include \
-    -I/usr/include/x86_64-linux-gnu/ \
-    -Ilibs/opus/celt \
-    -Ilibs/opus/include \
-    -Ilibs/opus/silk \
-    libs/opus/celt/modes.c \
-    -o modes.bc
-
-clang \
-    --target=wasm32 \
-    -nostdlib -ffreestanding -fno-exceptions \
-    -emit-llvm -c \
-    -DOPUS_BUILD=1 \
-    -DUSE_ALLOCA=1 \
-    -O3 \
-    -I/usr/lib/llvm-13/include/c++/v1/ \
-    -I/usr/include \
-    -I/usr/include/x86_64-linux-gnu/ \
-    -Ilibs/opus/celt \
-    -Ilibs/opus/include \
-    -Ilibs/opus/silk \
-    libs/opus/src/opus_decoder.c \
-    -o opus_decoder.bc
-
-clang \
-    --target=wasm32 \
-    -nostdlib -ffreestanding -fno-exceptions \
-    -emit-llvm -c \
-    -DOPUS_BUILD=1 \
-    -DUSE_ALLOCA=1 \
-    -O3 \
-    -I/usr/lib/llvm-13/include/c++/v1/ \
-    -I/usr/include \
-    -I/usr/include/x86_64-linux-gnu/ \
-    -Ilibs/opus/celt \
-    -Ilibs/opus/include \
-    -Ilibs/opus/silk \
-    libs/opus/silk/PLC.c \
-    -o PLC.bc
+OPUS=""
+for FILE in src/opus.c src/opus_decoder.c \
+    celt/bands.c \
+    celt/celt.c \
+    celt/celt_decoder.c \
+    celt/celt_lpc.c \
+    celt/cwrs.c \
+    celt/entcode.c \
+    celt/entdec.c \
+    celt/entenc.c \
+    celt/kiss_fft.c \
+    celt/laplace.c \
+    celt/mdct.c \
+    celt/mathops.c \
+    celt/modes.c \
+    celt/pitch.c \
+    celt/quant_bands.c \
+    celt/rate.c \
+    celt/vq.c \
+    silk/bwexpander.c \
+    silk/bwexpander_32.c \
+    silk/CNG.c \
+    silk/code_signs.c \
+    silk/dec_API.c \
+    silk/decode_core.c \
+    silk/decode_frame.c \
+    silk/decode_indices.c \
+    silk/decode_parameters.c \
+    silk/decode_pitch.c \
+    silk/decode_pulses.c \
+    silk/decoder_set_fs.c \
+    silk/gain_quant.c \
+    silk/init_decoder.c \
+    silk/log2lin.c \
+    silk/LPC_analysis_filter.c \
+    silk/LPC_fit.c \
+    silk/LPC_inv_pred_gain.c \
+    silk/NLSF_decode.c \
+    silk/NLSF_stabilize.c \
+    silk/NLSF_unpack.c \
+    silk/NLSF2A.c \
+    silk/pitch_est_tables.c \
+    silk/PLC.c \
+    silk/resampler.c \
+    silk/resampler_private_AR2.c \
+    silk/resampler_private_down_FIR.c \
+    silk/resampler_private_IIR_FIR.c \
+    silk/resampler_private_up2_HQ.c \
+    silk/resampler_rom.c \
+    silk/shell_coder.c \
+    silk/sort.c \
+    silk/stereo_decode_pred.c \
+    silk/stereo_MS_to_LR.c \
+    silk/sum_sqr_shift.c \
+    silk/table_LSF_cos.c \
+    silk/tables_gain.c \
+    silk/tables_LTP.c \
+    silk/tables_NLSF_CB_NB_MB.c \
+    silk/tables_NLSF_CB_WB.c \
+    silk/tables_other.c \
+    silk/tables_pitch_lag.c \
+    silk/tables_pulses_per_block.c
+do {
+    clang \
+        --target=wasm32 \
+        -nostdlib -ffreestanding -fno-exceptions \
+        -emit-llvm -c \
+        -DOPUS_BUILD=1 \
+        -DUSE_ALLOCA=1 \
+        -O3 \
+        -I/usr/lib/llvm-13/include/c++/v1/ \
+        -I/usr/include \
+        -I/usr/include/x86_64-linux-gnu/ \
+        -Ilibs/opus/celt \
+        -Ilibs/opus/include \
+        -Ilibs/opus/silk \
+        libs/opus/$FILE \
+        -o $FILE.bc
+    OPUS="$OPUS $FILE.bc"
+}
+done
 
 clang++ \
     --target=wasm32 \
@@ -142,6 +98,7 @@ clang++ \
     -I/usr/include/x86_64-linux-gnu/ \
     -Ilibs/memory/include/ \
     -Ilibs/opus/include \
+    -Wl,-error-limit=0 \
     -Wl,--no-entry \
     -Wl,--export=__wasm_call_ctors \
     -Wl,--export=malloc \
@@ -151,7 +108,7 @@ clang++ \
     -Wl,--export=free_decoder \
     -Wl,--lto-O3 \
     -Wl,-z,stack-size=$[8 * 1024 * 1024] \
-    stdlib/atexit.bc stdlib/malloc.bc stdlib/memset.bc stdlib/new.bc \
-    celt.bc celt_decoder.bc CNG.bc dec_API.bc init_decoder.bc opus_decoder.bc modes.bc PLC.bc \
+    stdlib/atexit.bc stdlib/malloc.bc stdlib/maths.bc stdlib/memcpy.bc stdlib/memmove.bc stdlib/memset.bc stdlib/new.bc \
+    $OPUS \
     ./opus.cpp \
     -o opus.wasm
