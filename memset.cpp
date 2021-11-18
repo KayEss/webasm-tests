73278f224455c8eacb4bccda19d2db3906bddfb1
Kirit Sælensminde <kirit@kirit.com>
2021-11-18 17:51:22 +0700

    Basic memset implementation.

---
+++ memset.cpp
@@ -0,0 +1,9 @@
+#include <cstring>
+
+
+extern "C" void* memset(void * const dest, int const ch, std::size_t count) {
+    auto *d = reinterpret_cast<unsigned char *>(dest);
+    while (count--) {
+        *d++ = ch;
+    }
+}
